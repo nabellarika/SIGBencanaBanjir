6f6ce566651342e240070f2ad3292c20371958cf
nabellarika <60418099+nabellarika@users.noreply.github.com>
2021-07-04 12:17:36 +0700

    Add files via upload

---
+++ data/RUMAHSAKIT_5.js
@@ -0,0 +1,31 @@
+var json_RUMAHSAKIT_5 = {
+"type": "FeatureCollection",
+"name": "RUMAHSAKIT_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Bangunan": "Balai Pengobatan Lanal Lampung", "Kategori": "Rumah Sakit Khusus" }, "geometry": { "type": "Point", "coordinates": [ 105.324885400000085, -5.480107430999965 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "Bangunan": "Rumah Sakit Ibu dan Anak Restu Bunda", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.256594272000086, -5.447410414999979 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "Bangunan": "Kantor PMI Lampung", "Kategori": "Rumah Sakit Khusus" }, "geometry": { "type": "Point", "coordinates": [ 105.267038648000039, -5.442533183999956 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "Bangunan": "Rumah Sakit Ibu dan Anak Santa Anna Bandarlampung", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.267662231000088, -5.442386254999974 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "Bangunan": "Rumah Sakit Umum Daerah Dr A Dadi Tjokrodipo Bandarlampung", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.252392072000077, -5.436485058999949 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "Bangunan": "Rumah Sakit Assyifa Bandarlampung", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.270983379000086, -5.429783518999955 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "Bangunan": "Rumah Sakit Bumiwaras", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.250776567000059, -5.424948495999956 ] } },
+{ "type": "Feature", "properties": { "fid": "8", "Bangunan": "Rumah Sakit Dwi Laras Bandarlampung", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.250921583000036, -5.424769126999934 ] } },
+{ "type": "Feature", "properties": { "fid": "9", "Bangunan": "Rumah Sakit Ibu dan Anak Mutiara Putri", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.262503767000055, -5.422932181999954 ] } },
+{ "type": "Feature", "properties": { "fid": "10", "Bangunan": "Kantor PMI Bandarlampung", "Kategori": "Rumah Sakit Khusus" }, "geometry": { "type": "Point", "coordinates": [ 105.262875984000061, -5.420484361999968 ] } },
+{ "type": "Feature", "properties": { "fid": "11", "Bangunan": "Rumah Sakit Bersalin Anugerah Medika Lampung", "Kategori": "Rumah Sakit Khusus" }, "geometry": { "type": "Point", "coordinates": [ 105.26044199200004, -5.419755642999974 ] } },
+{ "type": "Feature", "properties": { "fid": "12", "Bangunan": "Rumah Sakit Graha Husada Tanjungagungraya", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.267497505000051, -5.413538183999947 ] } },
+{ "type": "Feature", "properties": { "fid": "13", "Bangunan": "Rumah Sakit Bersalin Xaverius Bandarlampung", "Kategori": "Rumah Sakit Khusus" }, "geometry": { "type": "Point", "coordinates": [ 105.257545220000054, -5.404944679999971 ] } },
+{ "type": "Feature", "properties": { "fid": "14", "Bangunan": "Rumah Sakit Umum Daerah Abdul Moeloek Lampung", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.258926633000044, -5.402926110999942 ] } },
+{ "type": "Feature", "properties": { "fid": "15", "Bangunan": "Rumah Sakit Tingkat IV Bandarlampung", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.258649357000081, -5.401761295999961 ] } },
+{ "type": "Feature", "properties": { "fid": "16", "Bangunan": "Kantor PMI Lampung", "Kategori": "Rumah Sakit Khusus" }, "geometry": { "type": "Point", "coordinates": [ 105.257649449000041, -5.400732214999948 ] } },
+{ "type": "Feature", "properties": { "fid": "17", "Bangunan": "Rumah Sakit Ibu dan Anak Betik Hati", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.266016568000055, -5.394111828999939 ] } },
+{ "type": "Feature", "properties": { "fid": "18", "Bangunan": "Rumah Sakit Ibu dan Anak Sinta Langkapura", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.226498061000086, -5.392555061999929 ] } },
+{ "type": "Feature", "properties": { "fid": "19", "Bangunan": "Rumah Sakit Advent Bandarlampung", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.26212713700005, -5.392093601999932 ] } },
+{ "type": "Feature", "properties": { "fid": "20", "Bangunan": "Rumah Sakit Urip Sumoharjo", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.276595205000035, -5.391089970999928 ] } },
+{ "type": "Feature", "properties": { "fid": "21", "Bangunan": "Pusat Kesehatan Hewan Bandarlampung", "Kategori": "Rumah Sakit Khusus" }, "geometry": { "type": "Point", "coordinates": [ 105.217797552000036, -5.390625349999937 ] } },
+{ "type": "Feature", "properties": { "fid": "22", "Bangunan": "Rumah Sakit Imanuel Bandarlampung", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.287550755000041, -5.386027953999928 ] } },
+{ "type": "Feature", "properties": { "fid": "23", "Bangunan": "Pelayanan dan Rehabilitasi Sosial Penyandang Cacat", "Kategori": "Rumah Sakit Khusus" }, "geometry": { "type": "Point", "coordinates": [ 105.221576863000053, -5.383106387999931 ] } },
+{ "type": "Feature", "properties": { "fid": "24", "Bangunan": "Rumah Sakit Bhayangkari Polda Lampung", "Kategori": "Rumah Sakit Umum" }, "geometry": { "type": "Point", "coordinates": [ 105.224263362000045, -5.374049911999975 ] } }
+]
+}
